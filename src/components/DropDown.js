df8695f38f86cac353a106ce7d95b32870c708e6
Rafael Reis <rafyangtj@gmail.com>
2024-09-11 15:49:19 -0300

    added-dropdown-page

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,5 +1,31 @@
-function DropDown() {
-   return <div>Dropdown...</div>;
+import { useState } from 'react';
+
+function DropDown({ options, value, onChange }) {
+   const [isOpen, setIsOpen] = useState(false);
+
+   const handleClick = () => {
+      setIsOpen(!isOpen);
+   };
+
+   const handleOptionClick = option => {
+      setIsOpen(false);
+      onChange(option);
+   };
+
+   const renderedOptions = options.map(option => {
+      return (
+         <div onClick={() => handleOptionClick(option)} key={option.value}>
+            {option.label}
+         </div>
+      );
+   });
+
+   return (
+      <div>
+         <div onClick={handleClick}>{value?.label || 'Select...'}</div>
+         {isOpen && <div>{renderedOptions}</div>}
+      </div>
+   );
 }
 
 export default DropDown;
